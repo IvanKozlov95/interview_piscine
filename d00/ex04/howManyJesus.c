ff0708f30f6a42ed0e5e5f338e0ed6f2ca190d46
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-24 09:50:15 -0700

    Setting strings to lower case for seach

--- d00/ex04/howManyJesus.c
+++ d00/ex04/howManyJesus.c
@@ -6,30 +6,42 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 09:17:01 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/04/24 09:25:56 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/04/24 09:49:25 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+void	tolowerstr(char *s, int l) {
+	for (int i = 0; i < l; i++) {
+		if (isalpha(s[i]))
+			s[i] = tolower(s[i]);
+	}
+}
 
 int		howManyJesus(char *bible, char *jesus) {
+	int		i;
+	char	*term;
 	int		thatMany;
 	int		term_len;
 	int		book_len;
-	int		i;
 
-	i = -1;
 	thatMany = 0;
 	term_len = strlen(jesus);
 	book_len = strlen(bible);
+	term = strdup(jesus);
+	tolowerstr(term, term_len);
+	tolowerstr(bible, book_len);
+	i = -1;
 	while (++i < book_len - term_len - 1) {
 		while (!isalpha(bible[i])) i++;
-		if (strncmp(bible + i, jesus, term_len) == 0)
+		if (strncmp(bible + i, term, term_len) == 0)
 			thatMany++;
-		// printf("Here\n");
 		while (bible[i] != ' ' && bible[i] != '\n') i++;
 	}
+	free(term);
 	return (thatMany);
 }
