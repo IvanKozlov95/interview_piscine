2acbfb335164199ef22023664ff1cc303ae48868
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-02 15:35:59 -0700

    ex01

--- d05/ex01/bestPrice.c
+++ d05/ex01/bestPrice.c
@@ -3,19 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   bestPrice.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 13:51:42 by ivankozlov        #+#    #+#             */
-/*   Updated: 2018/05/02 14:05:43 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2018/05/02 15:32:21 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "header.h"
 
-double	bestPricesRec(int size, double *prices, double max) {
+void	bestPricesRec(int size, double *prices, double current, double *max) {
+	if (size == 0) {
+		*max < current ? *max = current : (void)0;
+		return ;
+	} else {
+		for (int i = 1; i <= size; i++)
+			bestPricesRec(size - i, prices, current + prices[i], max);
+	}
 }
 
 double	bestPrice(int pizzaSize, double *prices) {
-	bestPricesRec(pizzaSize, prices, 0);
+	double	best = 0;
+	bestPricesRec(pizzaSize, prices, 0, &best);
+	return (best);
 }
+	
