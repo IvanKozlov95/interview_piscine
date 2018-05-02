bc4661973dff3fb61fa95d1aa6e6d0f44b39622c
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-05-02 13:51:10 -0700

    ex00

--- d05/ex00/possibleSlices.c
+++ d05/ex00/possibleSlices.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   possibleSlices.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 13:15:31 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/05/02 13:25:56 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/05/02 13:46:54 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	printRec(t_array *a, int n) {
 		arrayPrint(a);
 		free(a);
 	} else {
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			t_array *clone = arrayClone(a);
 			arrayAppend(clone, i);
 			printRec(clone, n - i);
