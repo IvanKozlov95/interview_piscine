248e1ecfde173c4739acf38dd41e261c543d5be5
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-02 20:38:23 -0700

    ex02

--- d05/ex02/bestPriceV2.c
+++ d05/ex02/bestPriceV2.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/02 16:12:52 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/05/02 16:34:59 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/05/02 20:37:18 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@ double	findBestPriceRec(int size, double *prices, double *cache) {
 	if (cache[size])
 		return (cache[size]);
 	else
-		for (int i = 1; i <= size; i++) 
+		for (int i = 1; i < size; i++) 
 			if (findBestPriceRec(size - i, prices, cache) 
 				+ findBestPriceRec(i, prices, cache) > best)
-				// best = cache[size - i] + cache[i];
-				best++;
+				best = cache[size - i] + cache[i];
 	cache[size] = best;
 	return (best);
 }
