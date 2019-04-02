444bed10a64948c6246a074e9106abd1e2bc3b3c
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-04-02 12:38:57 -0700

    Removed comparation operator

--- d04/ex08/isFilled.c
+++ d04/ex08/isFilled.c
@@ -6,12 +6,12 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 12:31:25 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/04/02 12:32:46 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/04/02 12:38:40 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
 int isFilled(unsigned int parkingRow) {
-  return (parkingRow & (parkingRow + 1)) == 0;
+  return ~(parkingRow & (parkingRow + 1));
 }
